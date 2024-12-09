ca94d2be1c6f6586975edf794c1667a6619930f1
HwngBop <nguyendoduyhung697@gmail.com>
2024-12-09 12:01:34 +0700

    init

--- loginscript.js
+++ loginscript.js
@@ -4,15 +4,15 @@ function login() {
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
-    // Tìm người dùng trong mảng users
+    
     const user = users.find(user => user.email === email && user.password === password);
 
     if (user) {
         alert(`Chào mừng, ${user.username}! Bạn đã đăng nhập thành công.`);
         
-        // Chuyển hướng sang trang chủ (index.html)
-        window.location.href = "index.html"; // Chuyển hướng
-        // Lưu thông tin người dùng vào sessionStorage (hoặc localStorage nếu cần)
+        
+        window.location.href = "index.html"; 
+        
         sessionStorage.setItem('username', user.username);
     } else {
         alert('Email hoặc mật khẩu không đúng!');
@@ -24,9 +24,9 @@ function register() {
     const email = document.getElementById('register-email').value;
     const password = document.getElementById('register-password').value;
 
-    // Kiểm tra nếu người dùng điền đầy đủ thông tin
+    
     if (username && email && password) {
-        // Kiểm tra xem email đã tồn tại chưa
+        
         const existingUser = users.find(user => user.email === email);
 
         if (existingUser) {
@@ -37,9 +37,9 @@ function register() {
                 email: email,
                 password: password
             };
-            users.push(user); // Thêm vào mảng users
+            users.push(user); 
             alert("Đăng ký thành công!");
-            showLoginForm(); // Chuyển về form đăng nhập
+            showLoginForm(); 
         }
     } else {
         alert("Vui lòng điền đầy đủ thông tin!");
